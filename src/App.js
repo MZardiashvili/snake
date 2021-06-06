41126c149c2b655026407ba25ef75bdf59397964
Mariami-137 <M_Zardiashvili@outlook.com>
2021-06-06 21:40:52 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './board/Board';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Board></Board>
+    </div>
+  );
+}
+
+export default App;
